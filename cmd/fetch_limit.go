9f712e9c99175611f1a68940ab5e9eb175e1bfa2
benjamin.slabbert <benjamin.slabbert@gmail.com>
2022-02-05 19:11:18 +0100

    renamed cmd

--- cmd/fetch_limit.go
+++ cmd/fetch_limit.go
@@ -8,7 +8,7 @@ import (
 
 // fetchLimitCmd represents the fetchLimit command
 var fetchLimitCmd = &cobra.Command{
-	Use:   "fetchLimit",
+	Use:   "fetch-limit",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -17,7 +17,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("fetchLimit called")
+		fmt.Println("fetch-limit called")
 	},
 }
 
